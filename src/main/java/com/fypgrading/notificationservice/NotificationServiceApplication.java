b7f45ebf30af7014b163cf6d1c1b7df6cec1fa57
Gaelle327 <gaellesaid65@gmail.com>
2023-12-23 17:47:48 +0200

    adding aop for notification pipeline

--- src/main/java/com/fypgrading/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/fypgrading/notificationservice/NotificationServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(
 		scanBasePackages = {
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 		}
 )
 @EnableDiscoveryClient
+@EnableAspectJAutoProxy
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
